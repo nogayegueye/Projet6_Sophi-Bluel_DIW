f4092e8fd7ed1d785b3663e767701acc2ace567c
nogayegueye <nogayegye@gmail.com>
2023-03-04 02:59:04 +0100

    page connection

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,21 +1,40 @@
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 
-fetch('http://localhost:5678/api/works')
-    .then(response => response.json())
-    .then(data => {
-        let html = '';
+// fetch('http://localhost:5678/api/works')
+//     .then(response => response.json())
+//     .then(data => {
+//         let html = '';
 
-        data.forEach(item => {
-            html += `
-        <figure>
-          <img src="${item.imageUrl}" alt="${item.title}">
-          <figcaption>${item.title}</figcaption>
-        </figure>
-      `;
-        });
+//         data.forEach(item => {
+//             html += `
+//         <figure>
+//           <img src="${item.imageUrl}" alt="${item.title}">
+//           <figcaption>${item.title}</figcaption>
+//         </figure>
+//       `;
+//         });
+//       console.log("bonsoir");
+//         gallery.innerHTML = html;
+//     })
+//     .catch(error => console.error(error));
 
-        gallery.innerHTML = html;
-    })
-    .catch(error => console.error(error));
- 
+// console.log("bonjour");
+async function displayProjects() {
+  const response = await fetch("http://localhost:5678/api/works");
+  console.log(response);
+  const responseJson = await response.json();
+  console.log(responseJson);
+  let html = '';
+  responseJson.forEach((item) => {
+    html += `
+          <figure>
+            <img src="${item.imageUrl}" alt="${item.title}">
+            <figcaption>${item.title}</figcaption>
+          </figure>
+        `;
+  });
+  console.log("bonsoir");
+  gallery.innerHTML = html;
+}
 
+displayProjects();
